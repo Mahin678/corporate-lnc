cba530fea75c75588a43cd153042744456d4820c
Mahin Tazuar <mahintazuar678@gmail.com>
2022-01-01 23:54:42 +0600

    happy new year image update

--- src/Components/Home/Header/Header.js
+++ src/Components/Home/Header/Header.js
@@ -1,54 +1,68 @@
-import React from "react";
-import './Header.css'
-import Carousel from 'react-bootstrap/Carousel'
-const Header = () => {
-  return (
-    <div>
-      <Carousel indicators={false} interval="3000" >
-  <Carousel.Item>
-  <img
-              src="https://i.ibb.co/tmM2WGM/office.png"
-              className=" slide-img d-block w-100"
-              alt="..."
-            />
-            
-        
-    <Carousel.Caption>
-    <div className="carousel-caption ">
-              <h1>YOU CAN, IF YOU BELIVE YOU CAN</h1>
-              <h3>If people wanted to improve themselves instead of saving the whole world,</h3>
-            </div>
-    </Carousel.Caption>
-  </Carousel.Item>
-  <Carousel.Item>
-  <img
-              src="https://i.ibb.co/tmM2WGM/office.png"
-              className=" slide-img d-block w-100"
-              alt="..."
-            />
+import React from 'react';
+import './Header.css';
+import Carousel from 'react-bootstrap/Carousel';
+import img1 from '../../../image/business-woman-looking-professional-laptop-working-marketing-graphs-project.38b4f520.jpg';
+import img2 from '../../../image/landmarks-modern-city.5e2272d6.jpg';
+import img3 from '../../../image/people-business-meeting-high-angle.71049f7f.jpg';
+import img4 from '../../../image/New Year.png';
 
-    <Carousel.Caption>
-    <div className="carousel-caption  ">
-              <h1>YOU CAN, IF YOU BELIVE YOU CAN</h1>
-              <h3>If people wanted to improve themselves instead of saving the whole world,</h3>
-            </div>
-    </Carousel.Caption>
-  </Carousel.Item>
-  <Carousel.Item>
-  <img
-              src="https://i.ibb.co/tmM2WGM/office.png"
-              className=" slide-img d-block w-100"
-              alt="..."
-            />
-    <Carousel.Caption>
-    <div className="carousel-caption ">
-              <h1>YOU CAN, IF YOU BELIVE YOU CAN</h1>
-              <h3>If people wanted to improve themselves instead of saving the whole world,</h3>
-            </div>
-    </Carousel.Caption>
-  </Carousel.Item>
-</Carousel>
-      {/* <div
+const Header = () => {
+	let text =
+		'Let us communicate with the larger market of finance to ensure your capital flows in rhythm, while you can entirely focus on your other priorities... Let us like be your extended family who make sure that all your wings get stronger as each day passes by!';
+	return (
+		<div>
+			<Carousel indicators={false} interval="3000">
+				<Carousel.Item>
+					<img src={img4} className=" slide-img d-block w-100" alt="..." />
+
+					<Carousel.Caption>
+						<div className="py-5">
+							<div className="carousel-caption ">
+								<h1>
+									Happy New Year !! ,YOU CAN, IF YOU BELIEVE YOU CAN
+								</h1>
+								<h3>{text}</h3>
+							</div>
+						</div>
+					</Carousel.Caption>
+				</Carousel.Item>
+				<Carousel.Item>
+					<img src={img1} className=" slide-img d-block w-100" alt="..." />
+
+					<Carousel.Caption>
+						<div className="py-5">
+							<div className="carousel-caption ">
+								<h1>YOU CAN, IF YOU BELIEVE YOU CAN</h1>
+								<h3>{text}</h3>
+							</div>
+						</div>
+					</Carousel.Caption>
+				</Carousel.Item>
+				<Carousel.Item>
+					<img src={img2} className=" slide-img d-block w-100" alt="..." />
+
+					<Carousel.Caption>
+						<div className="py-5">
+							<div className="carousel-caption   ">
+								<h1>YOU CAN, IF YOU BELIEVE YOU CAN</h1>
+								<h3>{text}</h3>
+							</div>
+						</div>
+					</Carousel.Caption>
+				</Carousel.Item>
+				<Carousel.Item>
+					<img src={img3} className=" slide-img d-block w-100" alt="..." />
+					<Carousel.Caption>
+						<div className="py-5">
+							<div className="carousel-caption  ">
+								<h1>YOU CAN, IF YOU BELIEVE YOU CAN</h1>
+								<h3>{text}</h3>
+							</div>
+						</div>
+					</Carousel.Caption>
+				</Carousel.Item>
+			</Carousel>
+			{/* <div
         id="carouselExampleControls"
         className="carousel slide carousel-fade"
         data-ride="carousel"
@@ -113,8 +127,11 @@ const Header = () => {
           <span className="sr-only">Next</span>
         </a>
       </div> */}
-    </div>
-  );
+			<div className="text-center">
+				<img className="new-year-img" src={img4} alt="" />
+			</div>
+		</div>
+	);
 };
 
 export default Header;
